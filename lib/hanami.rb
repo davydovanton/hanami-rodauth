59fc4401f67c8c1acdecab0e98873260d329c232
Anton Davydov <antondavydov.o@gmail.com>
2016-10-08 23:37:51 +0300

    Create new cli command

--- lib/hanami.rb
+++ lib/hanami.rb
@@ -6,4 +6,16 @@ require 'hanami/cli_sub_commands/generate'
 
 module Hanami
 end
-Hanami::CliSubCommands::Generate.include Hanami::Auth
+
+Hanami::CliSubCommands::Generate.add_cli_commands do
+  desc 'auth', 'Generate an auth app'
+  long_desc <<-EOS
+  EOS
+  def auth
+    if options[:help]
+      invoke :help, ['auth']
+    else
+      Hanami::Commands::Auth.new(options).start
+    end
+  end
+end
